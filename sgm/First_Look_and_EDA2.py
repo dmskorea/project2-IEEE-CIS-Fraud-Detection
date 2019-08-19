f93cbbc0e82fb5400a6c115b0b4c6fc53a009f03
seuk <zeroshift01@naver.com>
2019-08-19 22:10:56 +0900

    eda2

---
+++ sgm/First_Look_and_EDA2.py
@@ -0,0 +1,85 @@
+import warnings
+
+import matplotlib.pylab as plt
+import numpy as np  # linear algebra
+import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
+import seaborn as sns
+
+warnings.simplefilter("ignore")
+plt.style.use('ggplot')
+color_pal = [x['color'] for x in plt.rcParams['axes.prop_cycle']]
+
+# Transaction CSVs
+train_transaction = pd.read_csv('C:/Users/seuk/git/project2-IEEE-CIS-Fraud-Detection/input/train_transaction.csv')
+test_transaction = pd.read_csv('C:/Users/seuk/git/project2-IEEE-CIS-Fraud-Detection/input/test_transaction.csv')
+# Identity CSVs - These will be merged onto the transactions to create additional features
+train_identity = pd.read_csv('C:/Users/seuk/git/project2-IEEE-CIS-Fraud-Detection/input/train_identity.csv')
+test_identity = pd.read_csv('C:/Users/seuk/git/project2-IEEE-CIS-Fraud-Detection/input/test_identity.csv')
+# Sample Submissions
+ss = pd.read_csv('C:/Users/seuk/git/project2-IEEE-CIS-Fraud-Detection/input/sample_submission.csv')
+
+print('train_transaction shape is {}'.format(train_transaction.shape))
+print('test_transaction shape is {}'.format(test_transaction.shape))
+print('train_identity shape is {}'.format(train_identity.shape))
+print('test_identity shape is {}'.format(test_identity.shape))
+
+print(np.sum(train_transaction['TransactionID'].unique()))
+
+# Here we confirm that all of the transactions in `train_identity`
+print(np.sum(train_transaction['TransactionID'].isin(train_identity['TransactionID'].unique())))
+print(np.sum(test_transaction['TransactionID'].isin(test_identity['TransactionID'].unique())))
+
+train_transaction['TransactionDT'].plot(kind='hist', figsize=(1, 1), label='train', bins=50, title='x')
+test_transaction['TransactionDT'].plot(kind='hist', label='test', bins=50)
+plt.legend()
+plt.show()
+
+ax = train_transaction.plot(x='TransactionDT',
+                       y='TransactionAmt',
+                       kind='scatter',
+                       alpha=0.01,
+                       label='TransactionAmt-train',
+                       title='Train and test Transaction Ammounts by Time (TransactionDT)',
+                       ylim=(0, 10000),
+                       figsize=(15, 5))
+test_transaction.plot(x='TransactionDT',
+                      y='TransactionAmt',
+                      kind='scatter',
+                      label='TransactionAmt-test',
+                      alpha=0.01,
+                      color=color_pal[1],
+                       ylim=(0, 10000),
+                      ax=ax)
+# Plot Fraud as Orange
+train_transaction.loc[train_transaction['isFraud'] == 1] \
+    .plot(x='TransactionDT',
+         y='TransactionAmt',
+         kind='scatter',
+         alpha=0.01,
+         label='TransactionAmt-train',
+         title='Train and test Transaction Ammounts by Time (TransactionDT)',
+         ylim=(0, 10000),
+         color='orange',
+         figsize=(15, 5),
+         ax=ax)
+plt.show()
+
+train_transaction['TransactionAmt'].apply(np.log)
+
+train_transaction['TransactionAmt'] \
+    .apply(np.log) \
+    .plot(kind='hist',
+          bins=100,
+          figsize=(15, 5),
+          title='Distribution of Log Transaction Amt')
+plt.show()
+
+train_transaction_fr = train_transaction.loc[train_transaction['isFraud'] == 1]
+train_transaction_nofr = train_transaction.loc[train_transaction['isFraud'] == 0]
+
+fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, figsize=(15, 8))
+train_transaction_fr.groupby('card4')['card4'].count().plot(kind='barh', ax=ax1, title='Count of card4 fraud')
+train_transaction_nofr.groupby('card4')['card4'].count().plot(kind='barh', ax=ax2, title='Count of card4 non-fraud')
+train_transaction_fr.groupby('card6')['card6'].count().plot(kind='barh', ax=ax3, title='Count of card6 fraud')
+train_transaction_nofr.groupby('card6')['card6'].count().plot(kind='barh', ax=ax4, title='Count of card6 non-fraud')
+plt.show()
