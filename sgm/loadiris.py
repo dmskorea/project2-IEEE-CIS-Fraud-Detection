5e1bfa424fc7900225bea6378463ad4532e91a70
seuk <zeroshift01@naver.com>
2019-08-21 17:16:17 +0900

    kfold 적용

--- sgm/loadiris.py
+++ sgm/loadiris.py
@@ -1,37 +1,33 @@
-from scipy.special.tests.test_sici import test_sici_consistency
-
 from sklearn.datasets import  load_iris
-from sklearn.model_selection import train_test_split
-from sklearn.neighbors.tests.test_nca import iris_data
+from sklearn.metrics import accuracy_score
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import KFold
 
+import numpy as np
 import pandas as pd
 
 iris = load_iris()
-iris.feature_names
+features = iris.data
+label = iris.target
+dt_clf = DecisionTreeClassifier(random_state=156)
 
-iris_data = iris.data
+kfold = KFold(n_splits=5)
+cv_accuracy = []
+print(features.shape[0])
 
-print(iris_data)
+n_iter = 0
 
-iris_data[0]
-
-iris_label = iris.target
-print(iris_label)
-print(iris.target)
-
-iris_df = pd.DataFrame(data=iris_data, columns=iris.feature_names)
-iris_df['lable'] = iris.target
-iris_df.head(3)
-
-X_train, X_test, y_train, y_test = train_test_split(iris_data, iris_label, test_size=0.2, random_state=11)
-
-dt_clf = DecisionTreeClassifier(random_state=11)
-
-
-dt_clf.fit(X_train, y_train)
-
-pred = dt_clf.predict(X_test)
-
-from sklearn.metrics import accuracy_score
-print(accuracy_score(y_test, pred))
+for train_index, test_index in kfold.split(features):
+    X_train, X_test = features[train_index], features[test_index]
+    y_train, y_test = label[train_index], label[test_index]
+    dt_clf.fit(X_train, y_train)
+    pred = dt_clf.predict(X_test)
+    n_iter += 1
+    accuracy = np.round(accuracy_score(y_test, pred), 4)
+    train_size = X_train.shape[0]
+    test_size = X_test.shape[0]
+    print(n_iter, accuracy, train_size, test_size)
+    print(n_iter, test_index)
+    cv_accuracy.append(accuracy)
+   
+np.mean(cv_accuracy)
