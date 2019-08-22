b47749775a9b5420bc5081961a1be031e66a4531
seuk <zeroshift01@naver.com>
2019-08-22 10:49:10 +0900

    cross_val_score

--- sgm/loadiris.py
+++ sgm/loadiris.py
@@ -5,29 +5,16 @@ from sklearn.model_selection import KFold
 
 import numpy as np
 import pandas as pd
+from sklearn.model_selection._validation import cross_val_predict,\
+    cross_val_score
 
 iris = load_iris()
-features = iris.data
-label = iris.target
 dt_clf = DecisionTreeClassifier(random_state=156)
 
-kfold = KFold(n_splits=5)
-cv_accuracy = []
-print(features.shape[0])
+data = iris.data
+label = iris.target
 
-n_iter = 0
+scores = cross_val_score(dt_clf, data, label, scoring='accuracy', cv=3)
 
-for train_index, test_index in kfold.split(features):
-    X_train, X_test = features[train_index], features[test_index]
-    y_train, y_test = label[train_index], label[test_index]
-    dt_clf.fit(X_train, y_train)
-    pred = dt_clf.predict(X_test)
-    n_iter += 1
-    accuracy = np.round(accuracy_score(y_test, pred), 4)
-    train_size = X_train.shape[0]
-    test_size = X_test.shape[0]
-    print(n_iter, accuracy, train_size, test_size)
-    print(n_iter, test_index)
-    cv_accuracy.append(accuracy)
-   
-np.mean(cv_accuracy)
+print(np.round(scores,4))
+print(np.round(np.mean(scores),4))
